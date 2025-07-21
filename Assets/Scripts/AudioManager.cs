5b08679f297ef53cdc6a79e4b0e2afea4d53dead
ironJohnsson <126124226+ironJohnsson@users.noreply.github.com>
2025-07-21 17:05:27 -0300

    aa

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+     [Header("Efeitos Sonoros")]
+    public AudioClip somDor;
+    public AudioClip somFalha;
+    public AudioClip somChoro;
+    public AudioClip somErro;
+    public AudioClip somEsforco;
+    public AudioClip somPulo;
+
+    private AudioSource audioSource;
+
+    void Awake()
+    {
+        // Singleton para acessar o manager de qualquer lugar
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void TocarSom(AudioClip som)
+    {
+        if (som != null)
+            audioSource.PlayOneShot(som);
+    }
+}
